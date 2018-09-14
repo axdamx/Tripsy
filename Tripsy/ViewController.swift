1bff6dfa014c3d0f22f57ff4e9f438a9a8a22cbe
Mohd Adam <adamode@mohds-macbook-pro.local>
2018-09-14 09:44:21 +0800

    Initial Commit

---
+++ Tripsy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tripsy
+//
+//  Created by Mohd Adam on 14/09/2018.
+//  Copyright © 2018 Mohd Adam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
